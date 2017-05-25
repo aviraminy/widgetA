fc80cee4874b3f3d3c0f36209fe0ba6e178d56a6
Alon Valadji <alon@ronin.co.il>
2017-05-25 16:26:49 +0300

    :poop: bundling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import _ from 'lodash';
-import moment from 'moment';
+import map from 'lodash/map';
+import reduce from 'lodash/reduce';
 
 function App() {
   return <div><h1>Hello I'm widgetA</h1></div>
